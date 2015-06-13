4f7071b6ccca1aea7727bd0fc91a72c27ed9835b
Andrew Gillis <andrew.gillis@spirent.com>
2015-06-13 10:09:01 -0700

    bump version for error handling change

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 def main():
     setup(
         name='stcrestclient',
-        version= '1.2.3',
+        version= '1.2.4',
         author='Andrew Gillis',
         author_email='andrew.gillis@spirent.com',
         url='https://github.com/Spirent/py-stcrestclient',
